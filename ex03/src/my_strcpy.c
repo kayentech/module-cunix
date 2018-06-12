79b25713c58468f830c7190272d49c4f1fd0f655
Denis Fedorchenko <dfedorchenko@heliostech.fr>
2018-06-12 10:14:29 +0300

    Refactored

--- ex03/src/my_strcpy.c
+++ ex03/src/my_strcpy.c
@@ -1,11 +1,5 @@
 char *my_strcpy(char *dest, const char *src) {
   char *ptr = dest;
-  while(*src != '\0') {
-    *ptr = *src;
-    ptr++;
-    src++;
-  }
-  *ptr = *src;
-
-  return dest;
+  while(*dest++ = *src++);
+  return ptr;
 }
